d4da1691ecf9886ea1ab83f6cdccd6c57f7b165f
Ray Cooper <rcooper1337@gmail.com>
2018-01-01 21:54:35 -0600

    Learning to read and write text files for the unscrambler game.

--- WordUnscrambler/Program.cs
+++ WordUnscrambler/Program.cs
@@ -1,28 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.IO;
 
 namespace WordUnscrambler
 {
     class Program
     {
-        public const string someText = "This is some text";
-
-        public static readonly string someOtherText = "This is some other text";
-
-        public static readonly Person someOtherPerson = new Person("A", "B");
-
         static void Main(string[] args)
         {
-            Console.WriteLine(someText);
-            Console.WriteLine(someOtherText);
+            // Creates a text file and saves all contents of the string array 
+            string[] lines = { "This is the first line", "This is the second line", "This is the third line" };
+            File.WriteAllLines("MyFirstFile.txt", lines);
+
+            string[] fileContents = File.ReadAllLines("MyFirstTextFile.txt");
+
+            foreach(string line in File.ReadLines("MyFirstFile.txt"))
+            {
+                Console.WriteLine(line);
+            }
         }
-
-        //static void ChangeName(Person personToChange)
-        //{
-        //    personToChange.FirstName = "Jane";
-        //    personToChange.LastName = "Doe";
-        //}
-
     }
 }
