3611be1539c6ece98732a70b2358189ebb0a1590
Ray Cooper <rcooper1337@gmail.com>
2018-01-01 19:32:26 -0600

    Day 1 of #100DaysOfCode

---
+++ WordUnscrambler/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WordUnscrambler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person();
+            person.FirstName = "John";
+            person.LastName = "Smith";
+
+            ChangeName(person);
+
+            Console.WriteLine(person.FirstName);
+            Console.WriteLine(person.LastName);
+
+            Console.ReadLine();
+        }
+
+        static void ChangeName(Person personToChange)
+        {
+            personToChange.FirstName = "Jane";
+            personToChange.LastName = "Doe";
+        }
+
+        static void ChangeNumber(ref int a)
+        {
+            a = 90;
+        }
+    }
+}
